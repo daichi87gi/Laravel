1ddd8d07aa863cc578148765395b25f26f402a8b
Daichi Yanagi <michaelyanagi@gmail.com>
2018-07-14 23:03:50 -0700

    Update routes admin group

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,26 @@ Route::get('/', function () {
 Route::get('post/{id}', function() {
     return view('blog.post');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('',function() {
+        return view('admin.index');
+    })->name('admin.index');
+
+    Route::get('create',function() {
+        return view('admin.create');
+    })->name('admin.create');
+
+    Route::post('create',function() {
+        return "It works!";
+    })->name('admin.create');
+
+    Route::get('edit/{id}', function() {
+        return view('admin.edit');
+    })->name('admin.edit');
+
+    Route::post('edit', function() {
+        return "It works!";
+    })->name('admin.update');
+
+});
