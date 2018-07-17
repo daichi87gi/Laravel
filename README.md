a04c7a550c3596d4726496c4bede7fa31e9a7cd6
Daichi Yanagi <michaelyanagi@gmail.com>
2018-07-17 01:17:22 -0700

    Add error handling Fix navigation bar (Using bootstrap 4.x) Update readme with some random notes

--- README.md
+++ README.md
@@ -12,3 +12,28 @@ https://github.com/barryvdh/laravel-ide-helper
 ## Useful Commands
 
 Clear the cash of views: `php artisan view:clear`
+
+## Random Notes
+
+### Request and Responses
+
+#### Dependency Injection vs Facades
+
+Dependency Injection (a good practise in general)
+
+`$request->input('mail')`
+
+Facades (/fa-sei-des/)
+
+`Request::input("mail')`
+
+#### RSRF Protection
+
+* `<input type="hidden" name="_token" value="{{ csrf_token() }}">`
+* Shorter syntax: `{{ csrf_field() }}`
+
+#### Save one-time data (called Sessions) in routes
+
+`return redirect()->route('admin.index')->with('info', 'Post edited, new Title: ' . $request->input('title'));`
+
+### Controllers and Models
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <title>That are Laravel Views with Laravel Blade</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
     <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
 </head>
 <body>
 @include('partials.admin-header')
